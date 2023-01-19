66ed37646d217366d42d3dda7e63e5fc702f630d
Adam Grzana <adamowiczkanapkowy@gmail.com>
2023-01-19 20:32:20 +0100

    Dodane wartosci do spinerów Wstepna funkcjonalnosc

--- app/src/main/java/com/example/myapplication/wybor_dan.java
+++ app/src/main/java/com/example/myapplication/wybor_dan.java
@@ -10,13 +10,20 @@ import java.util.List;
 import java.util.ListIterator;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
 
 public class wybor_dan extends AppCompatActivity {
-
+    TextView textview;
+    String Soup, Main_dish,Drinks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        new TworzenieDan();
+        textview=findViewById(R.id.Wynik);
         super.onCreate(savedInstanceState);
         pozyskajDania();
         setContentView(R.layout.activity_wybor_dan);
@@ -25,7 +32,51 @@ public class wybor_dan extends AppCompatActivity {
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        Spinner soup = (Spinner) findViewById(R.id.zupy);
+        Spinner main_plate = (Spinner) findViewById(R.id.dania_glowne);
+        Spinner drinks = (Spinner) findViewById(R.id.napoje);
+
+        ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,ZwróćNazweDania(zupy));
+        soup.setAdapter(adapter);
+        soup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(wybor_dan.this, "Selected " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                Soup= adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        ArrayAdapter adapter2 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,ZwróćNazweDania(daniaGlowne));
+        main_plate.setAdapter(adapter2);
+        main_plate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(wybor_dan.this, "Selected " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                Main_dish= adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        ArrayAdapter adapter3 = new ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item,ZwróćNazweDania(napoje));
+        drinks.setAdapter(adapter3);
+        drinks.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(wybor_dan.this, "Selected " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
+                Drinks= adapterView.getSelectedItem().toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
     }
+
     public List<Dania> zupy = new ArrayList<Dania>(){};
     public List<Dania> daniaGlowne = new ArrayList<Dania>(){};
     public List<Dania> napoje = new ArrayList<Dania>(){};
@@ -35,11 +86,13 @@ public class wybor_dan extends AppCompatActivity {
         zupy.add(new Dania("Pomidorowa", 15.90));
         zupy.add(new Dania("Ogórkowa", 13.90));
         zupy.add(new Dania("Rosół", 15));
+        zupy.add(new Dania("Brak", 0));
 
         daniaGlowne.add(new Dania("Kotlet Schabowy", 24.90));
         daniaGlowne.add(new Dania("Pierś z Kurczaka", 22.90));
         daniaGlowne.add(new Dania("Hamburger Wołowy", 23.90));
         daniaGlowne.add(new Dania("Sphagetti Bolognese", 23.90));
+        daniaGlowne.add(new Dania("Brak", 23.90));
 
         napoje.add(new Dania("Coca Cola 0,33", 4.90));
         napoje.add(new Dania("Sprite 0,33", 4.90));
@@ -48,4 +101,12 @@ public class wybor_dan extends AppCompatActivity {
         napoje.add(new Dania("Herbata owocowa", 6.90));
         napoje.add(new Dania("Sok świeżo wyciskany", 9.90));
     }
+    public String[] ZwróćNazweDania(List<Dania>nazwa){
+        String[] Names=new String[nazwa.size()];
+        for(int i=0;i<nazwa.size();i++){
+            Names[i]=nazwa.get(i).getNazwaDania();
+        }
+        return Names;
+    }
+
 }
