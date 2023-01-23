2a94eb89bf8f556893b68e8309fd4fd88c9bead8
Adam Grzana <adamowiczkanapkowy@gmail.com>
2023-01-23 14:34:51 +0100

    Dodane tworzneie zamówien dodawania nowych elementów edycja ceny

--- app/src/main/java/com/example/myapplication/wybor_dan.java
+++ app/src/main/java/com/example/myapplication/wybor_dan.java
@@ -1,5 +1,7 @@
 package com.example.myapplication;
 
+import static java.lang.Double.parseDouble;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -13,21 +15,29 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 
 public class wybor_dan extends AppCompatActivity {
-    TextView textview;
+    TextView textview,Cena;
+    EditText editzupy,editdania,editnapoje;
     String Soup, Main_dish,Drinks;
+    Button OrderButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        textview=findViewById(R.id.Wynik);
         super.onCreate(savedInstanceState);
         pozyskajDania();
         setContentView(R.layout.orders);
-
+        OrderButton=findViewById(R.id.orderbutton);
+        textview=findViewById(R.id.Wynik);
+        editzupy=findViewById(R.id.Ilość_zup);
+        editdania=findViewById(R.id.Ilość_dańgłównych);
+        editnapoje=findViewById(R.id.Ilość_Napoi);
+        Cena=findViewById(R.id.Cena);
         ActionBar actionBar=getSupportActionBar();
         if(actionBar!=null){
             actionBar.setDisplayHomeAsUpEnabled(true);
@@ -41,7 +51,6 @@ public class wybor_dan extends AppCompatActivity {
         soup.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(wybor_dan.this, "Selected " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
                 Soup= adapterView.getSelectedItem().toString();
             }
 
@@ -54,7 +63,6 @@ public class wybor_dan extends AppCompatActivity {
         main_plate.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(wybor_dan.this, "Selected " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
                 Main_dish= adapterView.getSelectedItem().toString();
             }
 
@@ -67,12 +75,51 @@ public class wybor_dan extends AppCompatActivity {
         drinks.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Toast.makeText(wybor_dan.this, "Selected " + adapterView.getSelectedItem().toString(), Toast.LENGTH_SHORT).show();
                 Drinks= adapterView.getSelectedItem().toString();
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
+        OrderButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String[] Zupy=ZwróćNazweDania(zupy);
+                String[] Dania=ZwróćNazweDania(daniaGlowne);
+                String[] Napoje=ZwróćNazweDania(napoje);
+                String Zawartosc="";
+                String pomoc=textview.getText().toString();
+                double Suma=0;
+                if(Cena.getText().toString().length()>0) {
+                    Suma= parseDouble(Cena.getText().toString());
+                }
+                if(Soup!="Brak") {
+                    for (int x = 0; x < Zupy.length; x++) {
+                        if (Soup.equals(Zupy[x])) {
+                            Zawartosc+=(Soup + " " + ZwróćCeneDania(zupy)[x] + " x " + editzupy.getText().toString()+"\n");
+                            Suma+=ZwróćCeneDania(zupy)[x]*parseDouble(editzupy.getText().toString());
+                        }
+                    }
+                }
+                if(Main_dish!="Brak") {
+                    for (int x = 0; x < Dania.length; x++) {
+                        if (Main_dish.equals(Dania[x])) {
+                            Zawartosc+=(Main_dish + " " + ZwróćCeneDania(daniaGlowne)[x] + " x " + editdania.getText().toString()+"\n");
+                            Suma+=ZwróćCeneDania(daniaGlowne)[x]*parseDouble(editdania.getText().toString());
+                        }
+                    }
+                }
+                if(Drinks!="Brak") {
+                    for (int x = 0; x < Napoje.length; x++) {
+                        if (Drinks.equals(Napoje[x])) {
+                            Zawartosc+=(Drinks + " " + ZwróćCeneDania(napoje)[x] + " x " + editnapoje.getText().toString()+"\n");
+                            Suma+=ZwróćCeneDania(napoje)[x]*parseDouble(editnapoje.getText().toString());
+                        }
+                    }
+                }
+                textview.setText(pomoc+Zawartosc);
+                Cena.setText(Double.toString(Suma));
             }
         });
     }
@@ -82,18 +129,18 @@ public class wybor_dan extends AppCompatActivity {
     public List<Dania> napoje = new ArrayList<Dania>(){};
 
     public void pozyskajDania(){
-
+        zupy.add(new Dania("Brak", 0));
         zupy.add(new Dania("Pomidorowa", 15.90));
         zupy.add(new Dania("Ogórkowa", 13.90));
         zupy.add(new Dania("Rosół", 15));
-        zupy.add(new Dania("Brak", 0));
 
+        daniaGlowne.add(new Dania("Brak", 0));
         daniaGlowne.add(new Dania("Kotlet Schabowy", 24.90));
         daniaGlowne.add(new Dania("Pierś z Kurczaka", 22.90));
         daniaGlowne.add(new Dania("Hamburger Wołowy", 23.90));
         daniaGlowne.add(new Dania("Sphagetti Bolognese", 23.90));
-        daniaGlowne.add(new Dania("Brak", 23.90));
 
+        napoje.add(new Dania("Brak", 0));
         napoje.add(new Dania("Coca Cola 0,33", 4.90));
         napoje.add(new Dania("Sprite 0,33", 4.90));
         napoje.add(new Dania("Fanta 0,33", 4.90));
@@ -108,5 +155,13 @@ public class wybor_dan extends AppCompatActivity {
         }
         return Names;
     }
+    public double[] ZwróćCeneDania(List<Dania>nazwa){
+        double[] Prices=new double[nazwa.size()];
+        for(int i=0;i<nazwa.size();i++){
+            Prices[i]=nazwa.get(i).getCenaDania();
+        }
+        return Prices;
+    }
+
 
 }
